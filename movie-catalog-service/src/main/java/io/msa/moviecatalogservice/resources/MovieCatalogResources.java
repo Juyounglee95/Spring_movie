3dccab6d5d8cce75a824f73b6bea7ad789f274d9
10190 <10190@SKCC19N00562.SKCC.NET>
2019-07-29 14:38:31 +0900

    using bean to create restTemplate instance

--- movie-catalog-service/src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResources.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,14 +18,12 @@ import io.msa.moviecatalogservice.models.Rating;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@RequestMapping("/{userId}")
 	public List<Catalogitem> getCatalog (@PathVariable("userId") String userId){
 		
-		RestTemplate restTemplate = new RestTemplate();
-		//restTemplate.getForObject("http://localhost:8081/movies/foo", Movie.class);
-		
-		//get all rated movie IDs
 		List<Rating> ratings = Arrays.asList(
 				new Rating("1234", 4),
 				new Rating("5678", 3)
