78f39822320379e2071da234653e97eef20810fb
10190 <10190@SKCC19N00562.SKCC.NET>
2019-07-29 13:35:24 +0900

    init
    setting server port 8083

---
+++ rating-date-service/src/main/java/io/msa/ratingdateservice/RatingDateServiceApplication.java
@@ -0,0 +1,13 @@
+package io.msa.ratingdateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingDateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingDateServiceApplication.class, args);
+	}
+
+}
