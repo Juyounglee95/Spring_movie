c977fc0c8fb8b2f2aa0200d3e8118a583c2dad7d
10190 <10190@SKCC19N00562.SKCC.NET>
2019-07-29 15:55:40 +0900

    Using WebClient to make API calls

--- movie-catalog-service/src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/msa/moviecatalogservice/resources/MovieCatalogResources.java
@@ -6,10 +6,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.msa.moviecatalogservice.models.Catalogitem;
 import io.msa.moviecatalogservice.models.Movie;
@@ -21,8 +23,12 @@ public class MovieCatalogResources {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
 	@RequestMapping("/{userId}")
 	public List<Catalogitem> getCatalog (@PathVariable("userId") String userId){
+		
+		WebClient.Builder builder = WebClient.builder();
 		
 		List<Rating> ratings = Arrays.asList(
 				new Rating("1234", 4),
@@ -31,8 +37,13 @@ public class MovieCatalogResources {
 		
 		return ratings.stream().map(rating->{
 			
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
-			
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = webClientBuilder.build()
+						.get()
+						.uri("http://localhost:8082/movies/"+rating.getMovieId())
+						.retrieve()
+						.bodyToMono(Movie.class)
+						.block();
 			return new Catalogitem(movie.getName(), "Description", rating.getRating());
 		})
 		.collect(Collectors.toList());
